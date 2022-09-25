30e1550d655e48eb468b7523904c9e98044a7baa
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-09-25 20:02:18 +0900

    feature: post prompt

--- discord_bot_client/src/commands/ai.rs
+++ discord_bot_client/src/commands/ai.rs
@@ -3,6 +3,7 @@ use std::time::Duration;
 
 use duct::cmd;
 use nix::unistd::{self, ForkResult};
+use serde_json::json;
 use serenity::framework::standard::macros::group;
 use serenity::framework::standard::{macros::command, Args, CommandResult};
 use serenity::http::CacheHttp;
@@ -18,7 +19,27 @@ pub struct Ai;
 
 #[command]
 pub async fn draw(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    log::info!("{:?}", args.message());
+    let arg = args.message();
+    log::info!("{:?}", arg);
+    if arg.is_empty() {
+        msg.channel_id
+            .send_message(&ctx.http, |m| {
+                m.embed(|e| {
+                    e.title("引数を与えてね")
+                        .field("Example", "~ai draw cat", false)
+                })
+            })
+            .await?;
+
+        return Ok(());
+    }
+
+    let client = reqwest::Client::new();
+    client
+        .post("https://k5vi72fcdo5u6gjqmuaqu5yoba0draxm.lambda-url.ap-northeast-1.on.aws/prompt")
+        .json(&json!({ "prompt": arg }))
+        .send()
+        .await?;
 
     match unsafe { unistd::fork() } {
         Ok(ForkResult::Parent { child, .. }) => {
