5bdcc698c41f0ee5aebbb8ab9309f70f7b78607f
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-09-29 01:02:43 +0900

    fix: colab disconnect, connect to gdrive

--- colab-cli/main.py
+++ colab-cli/main.py
@@ -8,14 +8,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 PROMPT_ENDPOINT = "https://k5vi72fcdo5u6gjqmuaqu5yoba0draxm.lambda-url.ap-northeast-1.on.aws/prompt"
-TIMEOUT = 12
+TIMEOUT = 20
 
 DRIVER_PATH = os.environ["DRIVER_PATH"]
 FIREFOX_PROFILE = os.environ["FIREFOX_PROFILE"]
 
 print("[INFO] run colab-cli")
 options = webdriver.FirefoxOptions()
-options.headless = True
+options.headless = False
 options.add_argument('--no-sandbox')
 profile = webdriver.FirefoxProfile(FIREFOX_PROFILE)
 
@@ -27,14 +27,62 @@ with webdriver.Firefox(options=options, firefox_profile=profile,
     driver.save_screenshot("google_colab.png")
 
     # すべて実行
-    all_exe = driver.find_element(
+    runtime = driver.find_element(
         by=By.CSS_SELECTOR, value="#runtime-menu-button > div > div > div.goog-inline-block.goog-menu-button-caption")
-    driver.maximize_window()
+    # driver.maximize_window()
     # all_exe.send_keys(Keys.CONTROL, Keys.F9)
-    all_exe.click()
+    runtime.click()
     time.sleep(1)
     all_exe = driver.find_element(by=By.CSS_SELECTOR, value="#\:1y")
     all_exe.click()
+
+    timeout_counter = 0
+    wait_for_secs = 2
+    while timeout_counter < 60 / wait_for_secs:
+        time.sleep(wait_for_secs)
+        try:
+            dialog = driver.find_element(
+                by=By.CSS_SELECTOR, value="body > colab-dialog > paper-dialog")
+            break
+        except:
+            pass
+        timeout_counter += 1
+    print("dialog")
+
+    timeout_counter = 0
+    wait_for_secs = 2
+    while timeout_counter < 60 / wait_for_secs:
+        time.sleep(wait_for_secs)
+        try:
+            connect_gdrive = driver.find_element(
+                by=By.CSS_SELECTOR, value="#ok")
+            connect_gdrive.click()
+            print("connect gdrive")
+            break
+        except:
+            pass
+        timeout_counter += 1
+
+    time.sleep(3)
+
+    driver.switch_to.window(driver.window_handles[-1])
+
+    select_account = driver.find_element(by=By.CSS_SELECTOR, value=".d2laFc")
+    select_account.click()
+
+    timeout_counter = 0
+    wait_for_secs = 2
+    while timeout_counter < 60 / wait_for_secs:
+        time.sleep(wait_for_secs)
+        try:
+            submit_approve_access = driver.find_element(
+                by=By.CSS_SELECTOR, value="#submit_approve_access > div:nth-child(1) > button:nth-child(1) > span:nth-child(4)")
+            submit_approve_access.click()
+            break
+        except:
+            pass
+        timeout_counter += 1
+
     time.sleep(180)
     timeout_counter = 0
     while True:
@@ -46,7 +94,34 @@ with webdriver.Firefox(options=options, firefox_profile=profile,
 
         prompt = requests.get(PROMPT_ENDPOINT).json()["prompt"]
         if prompt == "~completed":
+
             break
         time.sleep(10)
+
+    driver.switch_to.window(driver.window_handles[-1])
+
+    runtime = driver.find_element(
+        by=By.CSS_SELECTOR, value="#runtime-menu-button > div > div > div.goog-inline-block.goog-menu-button-caption")
+    runtime.click()
+    print("runtime")
+    time.sleep(1)
+
+    disconnect = driver.find_element(
+        by=By.CSS_SELECTOR, value="#\:27")
+    disconnect.click()
+    print("disconnect")
+    time.sleep(1)
+
+    dialog = driver.find_element(
+        by=By.CSS_SELECTOR, value="body > colab-dialog > paper-dialog > div.content-area > div")
+    dialog.click()
+    print("dialog")
+    time.sleep(1)
+
+    yes = driver.find_element(
+        by=By.CSS_SELECTOR, value="#ok")
+    yes.click()
+    print("yes")
+    time.sleep(1)
 
     driver.save_screenshot("exe.png")
