a3a03537d1a04f14daacc974889a6c3bdad021fb
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-10-04 00:21:18 +0900

    fix: hit weight

--- discord_bot_client/src/lib.rs
+++ discord_bot_client/src/lib.rs
@@ -116,7 +116,7 @@ impl EventHandler for Handler {
         // 会話AI
         {
             let choices = [true, false];
-            let weights = [0.20, 0.80];
+            let weights = [0.0005, 0.9995];
             let dist = WeightedIndex::new(&weights).unwrap();
             let mut rng = StdRng::from_rng(thread_rng()).unwrap();
 
